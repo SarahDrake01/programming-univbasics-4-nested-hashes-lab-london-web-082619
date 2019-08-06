096c984c195cd5df2b0e94b3d4efcf6a52f06956
Sarah Drake <jussaradrake@outlook.com>
2019-08-06 13:00:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,5 +132,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:language [1]] = "Assembly"
+programmer_hash[:dennis_ritchie][:language [1]] .merge = "Assembly"
 end
